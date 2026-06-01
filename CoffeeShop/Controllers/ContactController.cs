f32f6c8a001f37a69f8ad31d82f20211ec238b11
Prehistoric Programmer <tranvietkhang10a2@gmail.com>
2026-06-02 01:24:40 +0700

    Update Contact

---
+++ CoffeeShop/Controllers/ContactController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShop.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //public IActionResult Index(string name, string email, string message)
+        //{
+        //    ViewBag.Success = "Gửi thành công!";
+        //    return View();
+        //}
+    }
+}
